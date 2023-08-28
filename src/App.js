a91bd336892615a3ad2f24977ea1a399ea2186dd
unknown <limesoda314@gmail.com>
2023-08-28 16:53:41 -0700

    Updated routes, hopefully you can now directly navigate to a link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom"; 
+import { Routes, Route, BrowserRouter, Link } from "react-router-dom"; 
 import Home from "./Components/Pages/Home"; 
 import Projects from "./Components/Pages/Projects"; 
 import Gallery from "./Components/Pages/Gallery";
@@ -15,21 +15,22 @@ function App() {
    <div className="App">
     <BrowserRouter>       
         <Routes>         
-        <Route element={<Layout/>}>           
-            <Route path="/" element={<Home/>} />           
-            <Route path="projects" element={<Projects/>} />           
-            <Route path="gallery" element={<Gallery/>} />   
-            <Route path="blog" element={<Blog/>} />  
-            <Route path="*" element={<NoPage/>} />
+        <Route element={<Layout/>}>     
+             
+            <Route exact path="/" element={<Home/>} />           
+            <Route exact path="projects" element={<Projects/>} />           
+            <Route exact path="gallery" element={<Gallery/>} />   
+            <Route exact path="blog" element={<Blog/>} />  
+            <Route exact path="*" element={<NoPage/>} />
 
-            <Route path="projects/alphanumeric-handwriting-detector" element={<ProjectPages.Alphanumeric/>} />           
-            <Route path="projects/dspath-ucr-monet-poster" element={<ProjectPages.Poster/>} />   
-            <Route path="projects/cpp-forward-selection-and-backwards-elimination-implementation" element={<ProjectPages.ForwardBackwards/>} />  
-            <Route path="projects/cpp-nearest-neighbor-implementation" element={<ProjectPages.NearestNeighbor/>} />  
+            <Route exact path="projects/alphanumeric-handwriting-detector" element={<ProjectPages.Alphanumeric/>} />           
+            <Route exact path="projects/dspath-ucr-monet-poster" element={<ProjectPages.Poster/>} />   
+            <Route exact path="projects/cpp-forward-selection-and-backwards-elimination-implementation" element={<ProjectPages.ForwardBackwards/>} />  
+            <Route exact path="projects/cpp-nearest-neighbor-implementation" element={<ProjectPages.NearestNeighbor/>} />  
  
-            <Route path="blog/post1" element={<BlogPages.Post1/>} />       
-            <Route path="blog/post2" element={<BlogPages.Post2/>} />
-            <Route path="blog/post3" element={<BlogPages.Post3/>} />
+            <Route exact path="blog/post1" element={<BlogPages.Post1/>} />       
+            <Route exact path="blog/post2" element={<BlogPages.Post2/>} />
+            <Route exact path="blog/post3" element={<BlogPages.Post3/>} />
 
         </Route>    
         </Routes>     
