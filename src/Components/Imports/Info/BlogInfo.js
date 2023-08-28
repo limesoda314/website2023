50dcead56187be1b9787b4c6cc51643c2a63651f
unknown <limesoda314@gmail.com>
2023-08-28 16:43:44 -0700

    fixing build fail issue

--- src/Components/Imports/Info/BlogInfo.js
+++ src/Components/Imports/Info/BlogInfo.js
@@ -197,7 +197,7 @@ const BlogInfo = [
         href: 'post1',
         title: 'Post 25',
         date: '2023-06-06 00:00:00', 
-        author: 'Ann Miyaguchi', author: 'Ann Miyaguchi',
+        author: 'Ann Miyaguchi', 
         tags: ["tag1", "tag2",  "tag4"], 
         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
     },
